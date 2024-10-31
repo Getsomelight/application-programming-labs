68394a3b607a8164449ee7937deb05dc98488647
Getsomelight <maxvolyanyuk@gmail.com>
2024-10-31 15:28:31 +0400

    Lab_1

--- lab_1/main.py
+++ lab_1/main.py
@@ -13,28 +13,28 @@ def get_file()->str:
     except:
         raise SyntaxError("Path is empty")
 
-def open_file(str)->str:
+def open_file(name: str)->str:
     """
     Open and read the content of the file
     Raises FileNotFoundError if the file cannot be found
     """
     try:
-        with open(str, "r", encoding="UTF-8") as file:
+        with open(name, "r", encoding="UTF-8") as file:
             return file.read()
     except:
         raise FileNotFoundError("File doesnt found or doesnt exist")
 
-def find_fm_names(text)->list:
+def find_fm_names(data: str)->list:
     """
     Extract female names starting with 'А' from the text
     Returns a list of unique names
     """
     pattern = r'Имя: А\w+\nПол: Ж'
-    names = ''.join(re.findall(pattern, text))
+    names = ''.join(re.findall(pattern, data))
     pattern = r'А\w+'
-    answ = re.findall(pattern, names)
-    answ = list(set(answ))
-    return answ
+    answer = re.findall(pattern, names)
+    answer = list(set(answer))
+    return answer
 
 
 
